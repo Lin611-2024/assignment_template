fdc52da532044fc4a2a8e021d6f1f5267465372a
Josef Fruehwald <JoFrhwld@gmail.com>
2024-02-07 14:33:38 +0000

    pre-commit hook

--- .devcontainer/config.sh
+++ .devcontainer/config.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
 echo "Installing Developer Requirements"
 pip3 install --user -r requirements/requirements.txt
+echo "Merge Commits Only"
 git config pull.rebase false
+echo "pip freeze pre commit"
+ln .devcontainer/pre-commit .git/hooks/
+chmod +x .git/hooks/pre-commit
