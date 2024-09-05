5a7f7ecdfa1106d080f93e163c430df1dc161d29
Josef Fruehwald <JoFrhwld@gmail.com>
2024-09-05 15:47:31 -0400

    Update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,17 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
+lin611runtest::makesource(here::here("assignment.qmd"))
+
+readr::read_lines(here::here("tests", "R", "assignment.R")) |> 
+  stringr::str_replace(
+    "install.packages\\((.+)\\)",
+    "if\\(!require\\( \\1 \\)\\)\\{install.packages\\( \\1 \\)\\}"
+  ) |> 
+  stringr::str_replace(
+    "print\\(.+\\)",
+    ""
+  ) |> 
+  readr::write_lines(here::here("tests", "R", "assignment.R"))
 
 testthat::test_dir(here::here("tests", "testthat"))
-
